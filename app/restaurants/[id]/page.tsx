bcfc37e5b0a639e4b7b40142b5b0950ac4130333
Serg-ale <sergioalexandre0716@gmail.com>
2024-05-07 11:01:41 -0300

    feat: add restaurant page

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { db } from "@/app/_lib/prisma";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 import RestaurantImage from "../_components/restaurant-image";
-import RestaurantDetails from "../_components/restaurant-details";
+import RatingBadge from "@/app/_components/rating-badge";
+import CardEntrega from "@/app/_components/card-entrega";
+import ProductsList from "@/app/_components/products-list";
 
 interface RestaurantPageProps {
   params: {
@@ -15,7 +18,35 @@ const RestaurantsPage = async ({ params: { id } }: RestaurantPageProps) => {
       id,
     },
     include: {
-      categories: true,
+      categories: {
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          products: {
+            where: {
+              restaurantId: id,
+            },
+            include: {
+              restaurant: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      products: {
+        take: 10,
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
     },
   });
 
@@ -26,7 +57,59 @@ const RestaurantsPage = async ({ params: { id } }: RestaurantPageProps) => {
   return (
     <div>
       <RestaurantImage restaurant={restaurant} />
-      <RestaurantDetails restaurant={restaurant} />
+      <div className="z-90 relative mt-[-1.3rem] rounded-tl-3xl  rounded-tr-3xl bg-white py-5">
+        <div className="flex items-center px-5">
+          <div className="flex w-full items-center justify-between">
+            <div className="flex gap-2">
+              <div className="relative h-8 w-8">
+                <Image
+                  src={restaurant.imageUrl}
+                  alt={restaurant.name}
+                  fill
+                  className="rounded-full"
+                />
+              </div>
+              <h1 className="text-xl font-semibold">{restaurant.name}</h1>
+            </div>
+
+            <RatingBadge
+              backgroundColor="foreground"
+              textColor="white"
+              position=""
+            />
+          </div>
+        </div>
+
+        <div className="px-5">
+          <CardEntrega restaurant={restaurant} />
+        </div>
+
+        <div className="mt-3  flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+          {restaurant.categories.map((category) => (
+            <div
+              key={category.id}
+              className="min-w-[167px] rounded-lg bg-[#f4f4f4] text-center "
+            >
+              <span className="text-xs text-muted-foreground">
+                {category.name}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-6 space-y-4">
+          {/* TODO: Mostrar produtos mais pedidos quando implementarmos realização de pedido */}
+          <h2 className="px-5 font-semibold">Mais pedidos</h2>
+          <ProductsList products={restaurant.products} />
+        </div>
+
+        {restaurant.categories.map((category) => (
+          <div className="mt-6 space-y-4" key={category.id}>
+            <h2 className="px-5 font-semibold">{category.name}</h2>
+            <ProductsList products={category.products} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
